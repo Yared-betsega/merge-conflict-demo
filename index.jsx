6274d1e62633afbd002d20152837b8fd63b547b8
Yared Tsegaye <yaredtsegaye120@gmail.com>
2023-08-16 20:41:42 +0300

    add content 2

--- index.jsx
+++ index.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 const App = () => {
     return (
         <div>
-            <h1>Commit 1</h1>
-            <p>This is the content from the first commit.</p>
+            <h1>Commit 2</h1>
+            <p>This is the content from the second commit.</p>
         </div>
     );
 };
