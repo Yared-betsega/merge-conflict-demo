4c50b9a4203764b74e0f551215b0cda018830dfe
Yared Tsegaye <yaredtsegaye120@gmail.com>
2023-08-16 20:45:03 +0300

    add content 2

---
+++ file_from_this_branch.jsx
@@ -0,0 +1,13 @@
+// File: App.tsx
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Commit 2</h1>
+            <p>This is the content from the second commit.</p>
+        </div>
+    );
+};
+
+export default App;
