27a1111af97a615026601574ab4c7341927c95c9
Yared Tsegaye <yaredtsegaye120@gmail.com>
2023-08-16 20:34:11 +0300

    add index file

---
+++ index.jsx
@@ -0,0 +1,13 @@
+// File: App.tsx
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Commit 1</h1>
+            <p>This is the content from the first commit.</p>
+        </div>
+    );
+};
+
+export default App;
